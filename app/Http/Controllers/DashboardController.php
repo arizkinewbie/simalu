e7493a056785512875f8c472dd7bb681cd0bc094
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2024-09-16 00:46:33 +0700

    feat(dashboard): implement gate permissions

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Actions\Dashboard\Utils;
+use App\Constants\RBAC;
+use Illuminate\Support\Facades\Gate;
 
 class DashboardController extends Controller
 {
@@ -10,8 +12,12 @@ class DashboardController extends Controller
 
     public function index()
     {
+        if (!Gate::allows(sprintf('%s/%s', RBAC::PAGE_DASHBOARD, RBAC::SCOPE_READ))) {
+            abort(403);
+        }
+
         $greeting = $this->greetingByHour();
-        
+
         return view("dashboard.pages.index", compact("greeting"));
     }
 }
